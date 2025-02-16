67e612901cb93b9d457c23c6cb124f9eb8cde015
hugo-andriamaromanana <hugo.andriamaromanana@laplateforme.io>
2025-02-16 12:46:06 +0100

    [chore] refactor, lint

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from online_tictactoe.board.board import create_board
+from online_tictactoe.schemas.board import from_state
 from online_tictactoe.game.game import create_random_game
 from online_tictactoe.game.player import Player
+from online_tictactoe.schemas.cell_state import CellState
 
 
 def main():
     player1 = Player("Player1's name")
     player2 = Player("Player2's name")
-    board = create_board(3)
+    board = from_state(3, CellState.EMPTY)
     game = create_random_game(player1, player2, board)
     game.start()
     print(f"{player1.score = }, {player2.score = }")
+
 
 if __name__ == "__main__":
     main()
